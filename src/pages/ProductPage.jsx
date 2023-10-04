415b1b45436ab15a90d4766ad26324a0e4f56631
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-10-05 01:38:11 +0530

    added-subroute

--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,11 +1,10 @@
-// import React from 'react'
-
 import Layout from "../components/Layout"
+// TODO: @manohar On clicking the product card store its data in redux which will be used for populating here
 
 const ProductPage = () => {
   return (
     <Layout>
-ProductPage
+  ProductPage
     </Layout>
   )
 }
