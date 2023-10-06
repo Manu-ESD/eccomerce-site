66bd923af12aabedf800cb3f6bc636f7dd534f3e
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-10-07 03:18:00 +0530

    signup and signin layout updated

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,35 +1,40 @@
 import { Link } from "react-router-dom";
 const SignIn = () => {
   return (
-    <section className="flex justify-center items-center h-screen bg-gray-100">
-    <div className="max-w-md w-full bg-white rounded p-6 space-y-4">
-        <div className="mb-4">
-            <p className="text-gray-600 font-bold text-xl">Sign In</p>
-            <div>
-                <span>or</span>
-                <Link className="text-sm text-blue-600 hover:underline" to='/signup'> create an account</Link>
-            </div>
-        </div>
+<>
+<section className="flex justify-center items-center h-screen bg-gray-100">
+<div className="max-w-md w-full">
+<Link className="block text-sm text-blue-600 hover:underline mb-1" to='/home'> Back Home</Link>
+<div className="max-w-md w-full bg-white rounded p-6 space-y-4">
+<div className="mb-4">
+        <p className="text-gray-600 font-bold text-xl">Sign In</p>
         <div>
-            <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="email" placeholder="Email"/>
-        </div>
-        <div>
-            <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="password" placeholder="Password"/>
-        </div>
-        <div>
-            <button className="w-full py-4 bg-blue-600 hover:bg-blue-700 rounded text-sm font-bold text-gray-50 transition duration-200">Sign In</button>
-        </div>
-        <div className="flex items-center justify-between">
-            <div className="flex flex-row items-center">
-                <input type="checkbox" className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"/>
-                <label htmlFor="comments" className="ml-2 text-sm font-normal text-gray-600">Remember me</label>
-            </div>
-            <div>
-                <a className="text-sm text-blue-600 hover:underline" href="#">Forgot password?</a>
-            </div>
+            <span>or</span>
+            <Link className="text-sm text-blue-600 hover:underline" to='/signup'> create an account</Link>
         </div>
     </div>
+    <div>
+        <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="email" placeholder="Email"/>
+    </div>
+    <div>
+        <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="password" placeholder="Password"/>
+    </div>
+    <div>
+        <button className="w-full py-4 bg-blue-600 hover:bg-blue-700 rounded text-sm font-bold text-gray-50 transition duration-200">Sign In</button>
+    </div>
+    <div className="flex items-center justify-between">
+        <div className="flex flex-row items-center">
+            <input type="checkbox" className="focus:ring-blue-500 h-4 w-4 text-blue-600 border-gray-300 rounded"/>
+            <label htmlFor="comments" className="ml-2 text-sm font-normal text-gray-600">Remember me</label>
+        </div>
+        <div>
+            <a className="text-sm text-blue-600 hover:underline" href="#">Forgot password?</a>
+        </div>
+    </div>
+</div>
+</div>
 </section>
+</>
   )
 }
 
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,33 +1,71 @@
-
+import { Link } from "react-router-dom";
 const SignUp = () => {
-    return (
-      <section className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="max-w-md w-full bg-white rounded p-6 space-y-4">
+  return (
+    <section className="flex justify-center items-center h-screen bg-gray-100">
+      <div className="max-w-md w-full">
+        <Link
+          className="block text-sm text-blue-600 hover:underline mb-1"
+          to="/home"
+        >
+          {" "}
+          Back Home
+        </Link>
+        <div className="bg-white rounded p-6 space-y-4">
           <div className="mb-4">
-              <p className="text-gray-600">Sign Up</p>
-              <h2 className="text-xl font-bold">Join our community</h2>
+            <p className="text-gray-600">Sign Up</p>
+            <h2 className="text-xl font-bold">Join our community</h2>
           </div>
           <div>
-              <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="text" placeholder="First Name"/>
+            <input
+              className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+              type="text"
+              placeholder="First Name"
+            />
           </div>
           <div>
-              <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="text" placeholder="Last Name"/>
+            <input
+              className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+              type="text"
+              placeholder="Last Name"
+            />
           </div>
           <div>
-              <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="email" placeholder="Email"/>
+            <input
+              className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+              type="email"
+              placeholder="Email"
+            />
           </div>
           <div>
-              <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="password" placeholder="Password"/>
+            <input
+              className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+              type="password"
+              placeholder="Password"
+            />
           </div>
           <div>
-              <input className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600" type="password" placeholder="Confirm Password"/>
+            <input
+              className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+              type="password"
+              placeholder="Confirm Password"
+            />
           </div>
           <div>
-              <button className="w-full py-4 bg-blue-600 hover:bg-blue-700 rounded text-sm font-bold text-gray-50 transition duration-200">Create Account</button>
+            <button className="w-full py-4 bg-blue-600 hover:bg-blue-700 rounded text-sm font-bold text-gray-50 transition duration-200">
+              Create Account
+            </button>
           </div>
+          <div>
+            <Link to="/signin">
+              <button className="w-full py-4 bg-orange-500 hover:bg-orange-400 rounded text-sm font-bold text-gray-50 transition duration-200 shadow-md">
+                Existing User? Login
+              </button>
+            </Link>
+          </div>
+        </div>
       </div>
-  </section>
-    )
-  }
-  
-  export default SignUp
+    </section>
+  );
+};
+
+export default SignUp;
