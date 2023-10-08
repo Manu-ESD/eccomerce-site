413a33c976d6865de3b91fd92af33024fd5a590f
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-10-08 15:15:07 +0530

    forgot-password-implemented

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,37 +1,77 @@
 import { Link } from "react-router-dom";
 import { FiAlertCircle } from "react-icons/fi";
+import { passwordReset } from "../utility/utils";
+import { useState } from "react";
+
 const ForgotPassword = () => {
+  const [emailPasswordToBeReset, setEmailPasswordToBeReset] = useState("");
+  const [emailMessage, setEmailMessage] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      await passwordReset(emailPasswordToBeReset);
+      setEmailMessage(true);
+    } catch (error) {
+      if (error.code === "auth/user-not-found") {
+        alert("User not found, try again!");
+        setEmailPasswordToBeReset("");
+      }
+    }
+  };
+
   return (
     <>
       <section className="flex justify-center items-center h-screen bg-gray-100">
         <div className="max-w-md w-full">
-          <Link
-            className="block text-sm text-blue-600 hover:underline mb-1"
-            to="/signin"
-          >
-            {" "}
-            Back to Sign In
-          </Link>
-          <div className="max-w-md w-full bg-white rounded p-6 space-y-4">
-            <div className="flex justify-center">
-            <FiAlertCircle className="w-[80px] h-[80px]"  color="#0F65D9"/>
+          {emailMessage ? (
+            <div className="max-w-md w-full bg-white rounded p-6 space-y-4">
+              <h3>The Email has been sent; Check your Inbox!</h3>
             </div>
-           <p className="text-gray-600 font-bold text-xl text-center mb-4">Forgot Password</p>
-           <p className="text-gray-400 text-center font-semibold mb-2">Enter your email we'll send you a link to reset your password.</p>
+          ) : (
+            <>
+              <Link
+                className="block text-sm text-blue-600 hover:underline mb-1"
+                to="/signin"
+              >
+                {" "}
+                Back to Sign In
+              </Link>
+              <div className="max-w-md w-full bg-white rounded p-6 space-y-4">
+                <div className="flex justify-center">
+                  <FiAlertCircle
+                    className="w-[80px] h-[80px]"
+                    color="#0F65D9"
+                  />
+                </div>
+                <p className="text-gray-600 font-bold text-xl text-center mb-4">
+                  Forgot Password
+                </p>
+                <p className="text-gray-400 text-center font-semibold mb-2">
+                  Enter your email we&apos;ll send you a link to reset your
+                  password.
+                </p>
 
-            <div>
-              <input
-                className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
-                type="email"
-                placeholder="Email"
-              />
-            </div>
-            <div>
-              <button className="w-full py-4 bg-green-600 hover:bg-green-500 rounded text-sm font-bold text-gray-50 transition duration-200">
-                Submit
-              </button>
-            </div>
-          </div>
+                <div>
+                  <input
+                    className="w-full p-4 text-sm bg-gray-50 focus:outline-none border border-gray-200 rounded text-gray-600"
+                    type="email"
+                    placeholder="Email"
+                    onChange={(e)=>setEmailPasswordToBeReset(e.target.value)}
+                  />
+                </div>
+                <div>
+                  <button
+                    className="w-full py-4 bg-green-600 hover:bg-green-500 rounded text-sm font-bold text-gray-50 transition duration-200"
+                    onClick={handleSubmit}
+                  >
+                    Submit
+                  </button>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </section>
     </>
--- src/utility/utils.js
+++ src/utility/utils.js
@@ -1,21 +1,26 @@
-import { collection, getDocs,doc, setDoc } from "firebase/firestore";
-import { createUserWithEmailAndPassword,signInWithEmailAndPassword,signOut } from "firebase/auth";
-import { db,auth } from "../service";
-import { v4 as uuidv4 } from 'uuid';
+import { collection, getDocs, doc, setDoc } from "firebase/firestore";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+  sendPasswordResetEmail,
+  confirmPasswordReset,
+} from "firebase/auth";
+import { db, auth } from "../service";
+import { v4 as uuidv4 } from "uuid";
 import store from "../store";
 import { signOff } from "../features/authSlice";
 
-export const getDataFromFirebase = async(collectionName) => {
-  try{
+export const getDataFromFirebase = async (collectionName) => {
+  try {
     const querySnapshot = await getDocs(collection(db, collectionName));
-    if(!querySnapshot){
+    if (!querySnapshot) {
       throw new Error(`Unable to fetch data from ${collectionName}`);
     }
     const retrievedData = [];
     querySnapshot.forEach((doc) => retrievedData.push(doc.data()));
     return retrievedData;
-
-  }catch(err){
+  } catch (err) {
     console.error(err.message);
   }
 };
@@ -30,33 +35,32 @@ export const postProductsData = async (type, collectionName, productsData) => {
   }
 };
 
-export const getFiltersParams = (productsData)=>{
-  const brands=[];
-  const price=[];
-  const ratings=[];
-  const stock=[];
-  const discount=[];
-  const category=[];
-  
-  productsData.forEach((product)=>{
+export const getFiltersParams = (productsData) => {
+  const brands = [];
+  const price = [];
+  const ratings = [];
+  const stock = [];
+  const discount = [];
+  const category = [];
+
+  productsData.forEach((product) => {
     brands.push(product.brand);
     price.push(product.price);
     ratings.push(product.rating.rate);
     stock.push(product.stock);
     discount.push(product.discountPercentage);
     category.push(product.category);
-  })
+  });
 
   return {
-    brands:[...new Set(brands)],
-    price:[...new Set(price)],
-    ratings:[...new Set(ratings)],
-    stock:[...new Set(stock)],
-    discount:[...new Set(discount)],
-    category:[...new Set(category)]
-  }
-  
-}
+    brands: [...new Set(brands)],
+    price: [...new Set(price)],
+    ratings: [...new Set(ratings)],
+    stock: [...new Set(stock)],
+    discount: [...new Set(discount)],
+    category: [...new Set(category)],
+  };
+};
 
 export const signUpHandler = (email, password) => {
   createUserWithEmailAndPassword(auth, email, password)
@@ -69,9 +73,18 @@ export const signUpHandler = (email, password) => {
     });
 };
 
-export const signInHandler = ({email, password}) => signInWithEmailAndPassword(auth, email, password);
+export const signInHandler = ({ email, password }) => signInWithEmailAndPassword(auth, email, password);
+
 export const signUpWithFirebase = ({ email, password }) => createUserWithEmailAndPassword(auth, email, password);
+
 export const signOutWithFirebase = () => {
-  signOut(auth)
-  store.dispatch(signOff())
+  signOut(auth);
+  store.dispatch(signOff());
+};
+
+export const passwordReset = async (email) => await sendPasswordResetEmail(auth, email);
+
+export const confirmThePasswordReset = async (oobCode, newPassword) => {
+  if (!oobCode && !newPassword) return;
+  return await confirmPasswordReset(auth, oobCode, newPassword);
 };
