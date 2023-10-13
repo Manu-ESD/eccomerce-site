040d00090d5edd0d4fe745fc05f7f4c9ae6d2632
MASOOD AKHTAR VAHEED <vaheedsk36@gmail.com>
2023-10-13 21:37:10 +0530

    added-redux-persist

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
 import App from "./App.jsx";
 import store from "./store";
 import "./index.scss";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import { Provider } from 'react-redux';
+
+const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
+  <PersistGate loading={null} persistor={persistor}>
     <App />
+  </PersistGate>
 </Provider>
 );
--- src/store.js
+++ src/store.js
@@ -1,20 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers  } from "@reduxjs/toolkit";
 import { addToCartReducer } from "./features/cartSlice";
 import { productViewReducer } from "./features/productViewSlice";
 import { searchValueReducer } from "./features/searchValueSlice";
 import { authDataReducer } from "./features/authSlice";
 import { selectedCategoriesReducer } from "./features/selectedCategories";
 import { productsDataReducer } from "./features/productsSlice";
+import sessionStorage from "redux-persist/es/storage/session";
+import { persistReducer } from "redux-persist";
+
+
+const rootReducer = combineReducers({
+  addToCart: addToCartReducer,
+  productViewId: productViewReducer,
+  searchValue: searchValueReducer,
+  authData: authDataReducer,
+  selectedCategories: selectedCategoriesReducer,
+  productsData:productsDataReducer
+});
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage: sessionStorage,
+  blacklist: []
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const store = configureStore({
-  reducer: {
-    addToCart: addToCartReducer,
-    productViewId: productViewReducer,
-    searchValue: searchValueReducer,
-    authData: authDataReducer,
-    selectedCategories: selectedCategoriesReducer,
-    productsData:productsDataReducer
-  },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            immutableCheck: false,
+            serializableCheck: false,
+        }),
 });
 
 export default store;
